389c15fe4581e22ff15f06533b3ae6e5ba523bc1
Zihan Chen <zihanchen.ca@gmail.com>
2024-10-16 19:48:16 -0700

    fix comment

--- src/App.tsx
+++ src/App.tsx
@@ -24,22 +24,24 @@ function App() {
   return (
     <main>
       <h1> My personal web</h1>
-      // <h1>My todos</h1>
-      // <button onClick={createTodo}>+ new</button>
-      // <ul>
-      //   {todos.map((todo) => (
-      //     <li
-      //     onClick={() => deleteTodo(todo.id)}
-      //     key={todo.id}>{todo.content}</li>
-      //   ))}
-      // </ul>
-      // <div>
-      //   🥳 App successfully hosted. Try creating a new todo.
-      //   <br />
-      //   <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-      //     Review next step of this tutorial.
-      //   </a>
-      // </div>
+      /*
+      <h1>My todos</h1>
+      <button onClick={createTodo}>+ new</button>
+      <ul>
+        {todos.map((todo) => (
+          <li
+          onClick={() => deleteTodo(todo.id)}
+          key={todo.id}>{todo.content}</li>
+        ))}
+      </ul>
+      <div>
+        🥳 App successfully hosted. Try creating a new todo.
+        <br />
+        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
+          Review next step of this tutorial.
+        </a>
+      </div>
+      */
     </main>
   );
 }
