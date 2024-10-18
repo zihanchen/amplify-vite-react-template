b3a83ddcebf891bca0c523afb8e5789c13b981ae
Zihan Chen <zihanchen.ca@gmail.com>
2024-10-17 22:10:26 -0700

    fix null assertion

--- src/IntroTabs.tsx
+++ src/IntroTabs.tsx
@@ -20,7 +20,7 @@ function IntroTabs() {
       tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
     // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(cityName).style.display = "block";
+    document.getElementById(cityName)!.style.display = "block";
     evt.currentTarget.className += " active";
   }
 
