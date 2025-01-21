bfe338dc5c2d1488464971fba3bf7406b32eee67
Zihan Chen <zihanchen.ca@gmail.com>
2025-01-21 08:23:20 -0500

    Completed information in Experience, Education and Research

--- src/IntroTabs.tsx
+++ src/IntroTabs.tsx
@@ -30,32 +30,82 @@ function IntroTabs() {
       <div className="tab">
         <button className="tablinks" onClick={() => openCity(event, 'Experience')}>Experience</button>
         <button className="tablinks" onClick={() => openCity(event, 'Education')}>Education</button>
-        <button className="tablinks" onClick={() => openCity(event, 'Others')}>Others</button>
+        <button className="tablinks" onClick={() => openCity(event, 'Research')}>Research</button>
       </div>
 
       <div id="Experience" className="tabcontent">
         <h1>Amazon</h1>
-        <h3>Software Engineer, Amazon</h3>
+        <h4>2022, Sep - 2024, June</h4>
+        <h3>Software Engineer</h3>
         <p>- Responsible for maintaining data propagation and transformation service, performing oncall duties</p>
         <p>- Worked on history offload and unified propagation, which releases around 50% of the memory space in database</p>
         <p>- Developed self-service for Data Extraction requests, reduce manual requests by 80%</p>
 
         <h1>Coinbase</h1>
-        <h3>Software Engineer, Coinbase</h3>
+        <h4>2022, Feb - 2022, June</h4>
+        <h3>Software Engineer</h3>
         <p>- Responsible for maintaining data propagation and transformation service, performing oncall duties</p>
         <p>- Worked on history offload and unified propagation, which releases around 50% of the memory space in database</p>
         <p>- Developed self-service for Data Extraction requests, reduce manual requests by 80%</p>
+
+        <h1>Coherent Logix</h1>
+        <h4>2018, May - 2018, Sep</h4>
+        <h3>Software Engineer</h3>
+        <p>- Embed trained neural networks on low power consumption chips</p>
+        <p>- Add quantization to the net parser to improve performance by around 15%</p>
+        <p>- Widen the types of networks that can be processed</p>
+
+        <h1>Avidbots</h1>
+        <h4>2017, Sep - 2017, Dec</h4>
+        <h3>Software Engineer</h3>
+        <p>- Implement path planning algorithms for robots on ROS, adding A-star algorithm to the planning module</p>
+        <p>- Build new KPI for robots to measure performances, adding more data from sensor to improve evaluations for robots</p>
+        <p>- Built data pipeline for aggregating data from deployed robot</p>
 
       </div>
 
       <div id="Education" className="tabcontent">
         <h1>University of Waterloo</h1>
-        <p>To be continue</p>
+        <h4>2019, June - 2021, Dec</h4>
+        <p>Master of Mathematics - Computer Science</p>
+        <h3>Courses</h3>
+        <p>CS 854 - Advance Topics in Computer Science: IoT & Intelligent Connectivity</p>
+        <p>CS 798 - Advanced Research Topics: Network Softwarization</p>
+        <p>CS 680 - Introduction to Machine Learning</p>
+        <p>CS 885 - Advanced Topics in Computer Science: Reinforcement Learning</p>
+
+        <h1>University of Waterloo</h1>
+        <h4>2014, Sep - 2019, May</h4>
+        <p>Bachelor or Mathematics - Honours Computer Science & Honours Statistics with Computational Math Minor</p>
+
+        <h3>Highlighted Courses</h3>
+        <p>CS 246 - Object-Oriented Software Developed</p>
+        <p>CS 240 - Data Structure and Data Managemenet</p>
+        <p>CS 241 - Foundation of Sequential Programs</p>
+        <p>CS 350 - Operating Systems</p>
+        <p>STAT 341 - Computational Statistics & </p>
+        <p>CS 365 - Models of Computation</p>
+        <p>CS 343 - Concurrent and Parallel Programming</p>
+        <p>CS 454 - Distributed System</p>
+        <p>CS 450 - Computer Architecture</p>
+        <p>CS 456 - Computer Networks</p>
+        <p>CS 486 - Introduction to Artificial Intelligence</p>
+        <p>CO 487 - Applied Cryptography</p>
       </div>
 
-      <div id="Others" className="tabcontent">
-        <h3>To do</h3>
-        <p>Add some other projects</p>
+      <div id="Research" className="tabcontent">
+        <h1>Polite Wifi Sensing</h1>
+        <h4>2020, May - 2021, Sep</h4>
+        <p>Discover that private wifi network where IoT devices inside will respond to potentially malicious attacker </p>
+        <p>and detecting breath rate of people near target device by processing CSI info from responding packets.</p>
+        <p>Paper Reference: <a href="https://dspacemainprd01.lib.uwaterloo.ca/server/api/core/bitstreams/6c2877e0-7c01-4810-bba7-74a3e7e27ffc/content">Link</a></p>
+
+        <h1>VIVA: Road Condition Sensing Using Wireless Signals</h1>
+        <h4>2019, June - 2020, Apr</h4>
+        <p>Discovers that millimeter wave(mmWave) is able to enable robust and practical road sensing for road conditions.</p>
+        <p>Combining the mmWave radar with a machine learning algorithm, we trained a model that can detect the roughness of </p>
+        <p>the road, which can be used to detect the road condition.</p>
+        <p>Paper Reference: <a href="https://mobisys20.hotcrp.com/doc/mobisys20-paper213.pdf?cap=0213a1iBFyBH1Pys">Link</a></p>
       </div>
     </main>
   )
