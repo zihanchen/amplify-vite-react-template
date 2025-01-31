acfa0cc19c6df16728c1d815fa644c4acf4af899
Zihan Chen <zihanchen.ca@gmail.com>
2025-01-31 10:35:20 -0500

    remove unused imports

--- src/IntroTabs.tsx
+++ src/IntroTabs.tsx
@@ -2,31 +2,11 @@
 // import type { Schema } from "../amplify/data/resource";
 // import { generateClient } from "aws-amplify/data";
 // import NavBarHeader from "./ui-components/NavBarHeader";
-import { Flex, Divider, Heading, View, useTheme } from "@aws-amplify/ui-react";
+import { Flex, Divider, Heading, View } from "@aws-amplify/ui-react";
 import '@aws-amplify/ui-react/styles.css';
 import NavBarHeader from "./ui-components/NavBarHeader";
 
 function IntroTabs() {
-
-  function openTab(evt, tabName) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
-
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(tabName)!.style.display = "block";
-    evt.currentTarget.className += " active";
-  }
 
   return (
     <main>
